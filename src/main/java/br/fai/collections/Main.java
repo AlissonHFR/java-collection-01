516eaf04b1e0e233bc0df78bd5ba1218d9fb6150
Alisson <alissonhenrique98@gmail.com>
2020-03-09 20:30:53 -0300

    metodo obterValor

--- src/main/java/br/fai/collections/Main.java
+++ src/main/java/br/fai/collections/Main.java
@@ -1,5 +1,9 @@
 package br.fai.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,10 +12,25 @@ public class Main {
 		
 
 	}
+	
+	private Scanner ler = new Scanner(System.in);
 
 	private void start() {
-		// TODO Auto-generated method stub
+		int variavelLocal = 0;
 		
+		List<String> lista = new ArrayList<String>();
+		
+		String valorRecebido = obterDados();
+		System.out.println("Valor rebecebido foi: " + valorRecebido);
+		
+	}
+	
+	private String obterDados() {
+		System.out.println("Digite o valor: ");
+		
+		int valor = ler.nextInt();
+		
+		return String.valueOf(valor);
 	}
 
 }
